9100f617e7acbbf0e0adc8834fab8184d94bc64b
MksmOrlov <orlovmaksimkonstit@gmail.com>
2023-12-12 17:33:45 +0300

    feat: add docs, github, run and build scripts

--- scripts/set_vars.sh
+++ scripts/set_vars.sh
@@ -5,7 +5,9 @@ ROOT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pw
 
 export APP_ROOT_PATH="${APP_ROOT_PATH:-${ROOT_PATH}}"
 export METASYSTEM_PATH="${METASYSTEM_PATH:-${APP_ROOT_PATH}}"
-export PLATFORM_PATH="${PLATFORM_PATH:-${APP_ROOT_PATH}/ostis-web-platform}"
+export CONFIG_PATH="${CONFIG_PATH:-${METASYSTEM_PATH}/ostis-metasystem.ini}"
+export REPO_PATH_FILE="${REPO_PATH_FILE:-${METASYSTEM_PATH}/repo.path}"
+export PLATFORM_PATH="${PLATFORM_PATH:-${METASYSTEM_PATH}/ostis-web-platform}"
 
 if [[ -d "${PLATFORM_PATH}" ]];
 then
