0595a75377d525a9ee60214d06f535f9258f4adb
MksmOrlov <orlovmaksimkonstit@gmail.com>
2023-12-12 22:17:36 +0300

    refactor: add MIT Licence header in files

--- platform-dependent-components/problem-solver/py/modules/exampleModule/ExampleAgent.py
+++ platform-dependent-components/problem-solver/py/modules/exampleModule/ExampleAgent.py
@@ -1,3 +1,9 @@
+"""
+This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+Distributed under the MIT License
+(See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+"""
+
 import logging
 from sc_client.models import ScAddr, ScLinkContentType
 from sc_client.constants import sc_types
--- platform-dependent-components/problem-solver/py/tests/example_module/test_example_module.py
+++ platform-dependent-components/problem-solver/py/tests/example_module/test_example_module.py
@@ -1,3 +1,9 @@
+"""
+This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+Distributed under the MIT License
+(See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+"""
+
 from pathlib import Path
 
 from modules.exampleModule.ExampleModule import ExampleModule
