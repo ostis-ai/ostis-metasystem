aa65c5cf95203a9014097b642870702ab4578b8d
Anna Makarenko <manahafor4@gmail.com>
2023-12-15 11:15:25 +0300

    fix: string ScTypes

--- platform-dependent-components/problem-solver/cxx/identifiersModule/agent/FindIdentifiersAgent.hpp
+++ platform-dependent-components/problem-solver/cxx/identifiersModule/agent/FindIdentifiersAgent.hpp
@@ -20,68 +20,68 @@ class FindIdentifiersAgent : public ScAgent
 
   std::map<ScType, std::string> nodes = {
 
-      {ScType::Const, "node_const"},
-      {ScType::Var, "node_var"},
+      {ScType::Const, "sc_node"},
+      {ScType::Var, "sc_node"},
 
-      {ScType::Node, "node"},
-      {ScType::Link, "link"},
-      {ScType::LinkClass, "link_class"},
-      {ScType::Unknown, "unknown"},
+      {ScType::Node, "sc_node"},
+      {ScType::Link, "sc_link"},
+      {ScType::LinkClass, "sc_link"},
+      {ScType::Unknown, "sc_node"},
 
-      {ScType::NodeConst, "node_const"},
-      {ScType::NodeVar, "node_var"},
+      {ScType::NodeConst, "sc_node"},
+      {ScType::NodeVar, "sc_node"},
 
-      {ScType::LinkConst, "link_const"},
-      {ScType::LinkVar, "link_var"},
+      {ScType::LinkConst, "sc_link"},
+      {ScType::LinkVar, "sc_link"},
 
-      {ScType::NodeStruct, "node_struct"},
-      {ScType::NodeTuple, "node_tuple"},
-      {ScType::NodeRole, "node_role"},
-      {ScType::NodeNoRole, "node_no_role"},
-      {ScType::NodeClass, "node_class"},
-      {ScType::NodeAbstract, "node_abstract"},
-      {ScType::NodeMaterial, "node_material"},
+      {ScType::NodeStruct, "sc_node_struct"},
+      {ScType::NodeTuple, "sc_node_tuple"},
+      {ScType::NodeRole, "sc_node_role_relation"},
+      {ScType::NodeNoRole, "sc_node_norole_relatione"},
+      {ScType::NodeClass, "sc_node_not_relation"},
+      {ScType::NodeAbstract, "sc_node_abstract"},
+      {ScType::NodeMaterial, "sc_node_material"},
 
-      {ScType::NodeConstStruct, "node_const_struct"},
-      {ScType::NodeConstTuple, "node_const_tuple"},
-      {ScType::NodeConstRole, "node_const_role"},
-      {ScType::NodeConstNoRole, "node_const_no_role"},
-      {ScType::NodeConstClass, "node_const_class"},
-      {ScType::NodeConstAbstract, "node_const_abstract"},
-      {ScType::NodeConstMaterial, "node_const_material"},
+      {ScType::NodeConstStruct, "sc_node_struct"},
+      {ScType::NodeConstTuple, "sc_node_tuple"},
+      {ScType::NodeConstRole, "sc_node_role_relation"},
+      {ScType::NodeConstNoRole, "sc_node_norole_relation"},
+      {ScType::NodeConstClass, "sc_node_not_relation"},
+      {ScType::NodeConstAbstract, "sc_node"},
+      {ScType::NodeConstMaterial, "sc_node_material"},
 
-      {ScType::NodeVarStruct, "node_var_struct"},
-      {ScType::NodeVarTuple, "node_var_tuple"},
-      {ScType::NodeVarRole, "node_var_tuple"},
-      {ScType::NodeVarNoRole, "node_var_no_role"},
-      {ScType::NodeVarClass, "node_var_class"},
-      {ScType::NodeVarAbstract, "node_var_abstract"},
-      {ScType::NodeVarMaterial, "node_var_material"}};
+      {ScType::NodeVarStruct, "sc_node_struct"},
+      {ScType::NodeVarTuple, "sc_node_tuple"},
+      {ScType::NodeVarRole, "sc_node_role_relation"},
+      {ScType::NodeVarNoRole, "sc_node_norole_relation"},
+      {ScType::NodeVarClass, "sc_node_not_relation"},
+      {ScType::NodeVarAbstract, "sc_node"},
+      {ScType::NodeVarMaterial, "sc_node_material"}};
 
   std::map<ScType, std::string> edges = {
-      {ScType::EdgeUCommon, "edge_ucommon"},
-      {ScType::EdgeDCommon, "edge_dcommon"},
+      {ScType::EdgeUCommon, "sc_edge"},
+      {ScType::EdgeDCommon, "sc_edge_common"},
 
-      {ScType::EdgeUCommonConst, "edge_ucommon_const"},
-      {ScType::EdgeDCommonConst, "edge_dcommon_const"},
+      {ScType::EdgeUCommonConst, "<=>"},
+      {ScType::EdgeDCommonConst, "sc_edge_common"},
 
-      {ScType::EdgeAccess, "edge_access"},
+      {ScType::EdgeAccess, "sc_edge_access"},
 
-      {ScType::EdgeAccessConstPosPerm, "edge_access_const_pos_perm"},
-      {ScType::EdgeAccessConstNegPerm, "edge_access_const_neg_perm"},
-      {ScType::EdgeAccessConstFuzPerm, "edge_access_const_fuz_perm"},
-      {ScType::EdgeAccessConstPosTemp, "edge_access_const_pos_temp"},
-      {ScType::EdgeAccessConstNegTemp, "edge_access_const_neg_temp"},
-      {ScType::EdgeAccessConstFuzTemp, "edge_access_const_fuz_temp"},
+      {ScType::EdgeAccessConstPosPerm, "sc_edge_main"},
+      {ScType::EdgeAccessConstNegPerm, "sc_edge_main"},
+      {ScType::EdgeAccessConstFuzPerm, "sc_edge_main"},
+      {ScType::EdgeAccessConstPosTemp, "sc_edge_main"},
+      {ScType::EdgeAccessConstNegTemp, "sc_edge_main"},
+      {ScType::EdgeAccessConstFuzTemp, "sc_edge_main"},
 
-      {ScType::EdgeUCommonVar, "edge_ucommon_var"},
-      {ScType::EdgeDCommonVar, "edge_dcommon_var"},
-      {ScType::EdgeAccessVarPosPerm, "edge_access_var_pos_perm"},
-      {ScType::EdgeAccessVarNegPerm, "edge_access_var_neg_perm"},
-      {ScType::EdgeAccessVarFuzPerm, "edge_access_var_fuz_perm"},
-      {ScType::EdgeAccessVarPosTemp, "edge_access_var_pos_temp"},
-      {ScType::EdgeAccessVarNegTemp, "edge_access_var_neg_temp"},
-      {ScType::EdgeAccessVarFuzTemp, "edge_access_var_fuz_temp"}};
+      {ScType::EdgeUCommonVar, "_<=>"},
+      {ScType::EdgeDCommonVar, "sc_edge_common"},
+      {ScType::EdgeAccessVarPosPerm, "sc_edge_main"},
+      {ScType::EdgeAccessVarNegPerm, "sc_edge_main"},
+      {ScType::EdgeAccessVarFuzPerm, "sc_edge_main"},
+      {ScType::EdgeAccessVarPosTemp, "sc_edge_main"},
+      {ScType::EdgeAccessVarNegTemp, "sc_edge_main"},
+      {ScType::EdgeAccessVarFuzTemp, "sc_edge_main"}};
 
 private:
   bool checkAction(ScAddr const & actionAddr);
