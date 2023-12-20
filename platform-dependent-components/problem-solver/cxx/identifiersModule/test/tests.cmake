8bd2f249df65334ba31062f737b08aa4ae9c3972
Anna Makarenko <manahafor4@gmail.com>
2023-12-20 17:26:25 +0300

    fix: agent doesnt work

--- platform-dependent-components/problem-solver/cxx/identifiersModule/test/tests.cmake
+++ platform-dependent-components/problem-solver/cxx/identifiersModule/test/tests.cmake
@@ -1,11 +1,7 @@
-#target_link_libraries(
-#        sc-agents-common
-#)
-#
-#make_tests_from_folder(${CMAKE_CURRENT_LIST_DIR}/units
-#    NAME identifiers_module_test_starter
-#    DEPENDS sc-agents-common sc-builder-lib identifiers
-#    INCLUDES ${SC_MEMORY_SRC}/tests/sc-memory/_test ${CMAKE_CURRENT_LIST_DIR}/.. ${SC_MACHINE_ROOT}/sc-tools/sc-builder/src
-#)
-#
-#add_definitions(-DIDENTIFIERS_MODULE_TEST_SRC_PATH="${CMAKE_CURRENT_LIST_DIR}")
+make_tests_from_folder(${CMAKE_CURRENT_LIST_DIR}/units
+    NAME identifiers_module_test_starter
+    DEPENDS sc-agents-common sc-builder-lib identifiers
+    INCLUDES ${SC_MEMORY_SRC}/tests/sc-memory/_test ${CMAKE_CURRENT_LIST_DIR}/.. ${SC_MACHINE_ROOT}/sc-tools/sc-builder/src
+)
+
+add_definitions(-DIDENTIFIERS_MODULE_TEST_SRC_PATH="${CMAKE_CURRENT_LIST_DIR}")
