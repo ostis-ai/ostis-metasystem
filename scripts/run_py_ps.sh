dbfa127994edcae4867a0667de6cb46e02d9b9a8
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-03-05 20:16:00 +0300

    scripts: unify namings corresponding to component names

---
+++ scripts/run_py_ps.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/set_vars.sh"
+
+python3 "${METASYSTEM_PATH}/platform-dependent-components/problem-solver/py/server.py" "$@"
