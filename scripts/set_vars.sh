f8226f812128294cc13a1b5e2e322ce4e0a42ae8
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-03-05 20:16:58 +0300

    scripts: make var names unique

--- scripts/set_vars.sh
+++ scripts/set_vars.sh
@@ -3,15 +3,17 @@ set -eo pipefail
 
 ROOT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd)
 
-export APP_ROOT_PATH="${APP_ROOT_PATH:-${ROOT_PATH}}"
-export ROOT_CMAKE_PATH="${ROOT_CMAKE_PATH:-${APP_ROOT_PATH}}"
-export METASYSTEM_PATH="${METASYSTEM_PATH:-${APP_ROOT_PATH}}"
+export METASYSTEM_PATH="${METASYSTEM_PATH:-${ROOT_PATH}}"
+
+export METASYSTEM_CMAKE_PATH="${METASYSTEM_CMAKE_PATH:-${ROOT_PATH}}"
+export METASYSTEM_CXX_SOURCES_PATH="${METASYSTEM_PATH}/platform-dependent-components/problem-solver"
+export METASYSTEM_BUILD_PATH="${METASYSTEM_BUILD_PATH:-${METASYSTEM_CMAKE_PATH}/build}"
+export BINARY_PATH="${BINARY_PATH:-${METASYSTEM_CMAKE_PATH}/bin}"
+
 export CONFIG_PATH="${CONFIG_PATH:-${METASYSTEM_PATH}/ostis-metasystem.ini}"
 export REPO_PATH_FILE="${REPO_PATH_FILE:-${METASYSTEM_PATH}/repo.path}"
+
 export PLATFORM_PATH="${PLATFORM_PATH:-${METASYSTEM_PATH}/ostis-web-platform}"
-export BINARY_PATH="${BINARY_PATH:-${ROOT_CMAKE_PATH}/bin}"
-export BUILD_PATH="${BUILD_PATH:-${ROOT_CMAKE_PATH}/build}"
-export PROBLEM_SOLVER_PATH="${METASYSTEM_PATH}/platform-dependent-components/problem-solver"
 
 if [[ -d "${PLATFORM_PATH}" ]];
 then
