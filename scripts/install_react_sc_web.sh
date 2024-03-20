d8bfce335f594dccfd3c827c6b297a215ebde856
MksmOrlov <orlovmaksimkonstit@gmail.com>
2024-03-20 18:03:11 +0300

    refactor: rename install react deps script

--- scripts/install_react_sc_web.sh
+++ scripts/install_react_sc_web.sh
@@ -11,4 +11,4 @@ fi
 UTILS_DIR="${PLATFORM_PATH}/scripts/submodule-scripts/utils"
 "${UTILS_DIR}/install_submodule.sh" --repo "${REACT_SC_WEB_REPO}" --path "${REACT_SC_WEB_PATH}" --branch "${REACT_SC_WEB_BRANCH}" --commit "${REACT_SC_WEB_COMMIT}"
 
-"${REACT_SC_WEB_PATH}/scripts/install_react_sc_web.sh"
+"${REACT_SC_WEB_PATH}/scripts/install_react_sc_web_dependencies.sh"
