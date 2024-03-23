e59c1f2be55d57916163a405595fa335add45dea
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-03-24 02:59:20 +0300

    scripts: update submodules

--- scripts/set_vars.sh
+++ scripts/set_vars.sh
@@ -15,6 +15,21 @@ export REPO_PATH_FILE="${REPO_PATH_FILE:-${METASYSTEM_PATH}/repo.path}"
 
 export PLATFORM_PATH="${PLATFORM_PATH:-${METASYSTEM_PATH}/ostis-web-platform}"
 
+export SC_MACHINE_REPO="${SC_MACHINE_REPO:-https://github.com/NikitaZotov/sc-machine.git}"
+export SC_MACHINE_BRANCH="${SC_MACHINE_BRANCH:-fix/cmake_deps}"
+export SC_MACHINE_COMMIT="${SC_MACHINE_COMMIT:-""}"
+export SC_MACHINE_PATH="${SC_MACHINE_PATH:-${PLATFORM_PATH}/sc-machine}"
+
+export SCP_MACHINE_REPO="${SCP_MACHINE_REPO:-https://github.com/NikitaZotov/scp-machine.git}"
+export SCP_MACHINE_BRANCH="${SCP_MACHINE_BRANCH:-fix/cmake_deps}"
+export SCP_MACHINE_COMMIT="${SCP_MACHINE_COMMIT:-""}"
+export SCP_MACHINE_PATH="${SCP_MACHINE_PATH:-${PLATFORM_PATH}/scp-machine}"
+
+export SC_COMPONENT_MANAGER_REPO="${SC_COMPONENT_MANAGER_REPO:-https://github.com/NikitaZotov/sc-component-manager.git}"
+export SC_COMPONENT_MANAGER_BRANCH="${SC_COMPONENT_MANAGER_BRANCH:-fix/cmake_deps}"
+export SC_COMPONENT_MANAGER_COMMIT="${SC_COMPONENT_MANAGER_COMMIT:-""}"
+export SC_COMPONENT_MANAGER_PATH="${SC_COMPONENT_MANAGER_PATH:-${PLATFORM_PATH}/sc-component-manager}"
+
 if [[ -d "${PLATFORM_PATH}" ]];
 then
   source "${PLATFORM_PATH}/scripts/set_vars.sh"
