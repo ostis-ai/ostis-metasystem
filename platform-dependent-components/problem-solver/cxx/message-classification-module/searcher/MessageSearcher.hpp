d6eb379b4fe168f1564654c4ec55f779c9c1d2c1
Saymon <sem1235463782@gmail.com>
2024-04-02 09:07:03 +0300

    feat: add message classification module

---
+++ platform-dependent-components/problem-solver/cxx/message-classification-module/searcher/MessageSearcher.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "sc-memory/sc_addr.hpp"
+#include "sc-memory/sc_memory.hpp"
+
+namespace dialogControlModule
+{
+class MessageSearcher
+{
+public:
+  explicit MessageSearcher(ScMemoryContext * ms_context);
+
+  ScAddr getFirstMessage(ScAddr const & nonAtomicMessageNode);
+
+  ScAddr getNextMessage(ScAddr const & messageNode);
+
+  ScAddr getMessageAuthor(ScAddr const & messageNode);
+
+  ScAddr getMessageTheme(ScAddr const & messageNode);
+
+  ScAddrVector getMessageLinks(ScAddr const & message, ScAddrVector const & linkClasses = {});
+
+  ScAddr getMessageLink(ScAddr const & message, ScAddrVector const & linkClasses = {});
+
+private:
+  ScMemoryContext * context;
+};
+}  // namespace dialogControlModule
