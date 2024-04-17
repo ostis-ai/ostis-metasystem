eae798846a35179f30da9a80dc7072ee1ef01882
Andy <irarybakova90@gmial.com>
2024-04-17 15:19:56 +0300

    fixed(AskAi):keynodes

--- platform-dependent-components/problem-solver/cxx/message-reply-module/keynodes/MessageReplyKeynodes.hpp
+++ platform-dependent-components/problem-solver/cxx/message-reply-module/keynodes/MessageReplyKeynodes.hpp
@@ -19,28 +19,28 @@ class MessageReplyKeynodes : public ScObject
   SC_GENERATED_BODY()
 
 public:
-  SC_PROPERTY(Keynode("action_find_identifiers"), ForceCreate)
+  SC_PROPERTY(Keynode("action_reply_to_message"), ForceCreate)
   static ScAddr action_reply_to_message;
 
-  SC_PROPERTY(Keynode("action_find_identifiers"), ForceCreate)
+  SC_PROPERTY(Keynode("nrel_authors"), ForceCreate)
   static ScAddr nrel_authors;
 
-  SC_PROPERTY(Keynode("action_find_identifiers"), ForceCreate)
+  SC_PROPERTY(Keynode("nrel_reply"), ForceCreate)
   static ScAddr nrel_reply;
 
   SC_PROPERTY(Keynode("action_find_identifiers"), ForceCreate)
   static ScAddr myself;
 
-  SC_PROPERTY(Keynode("action_find_identifiers"), ForceCreate)
+  SC_PROPERTY(Keynode("message_processing_program"), ForceCreate)
   static ScAddr message_processing_program;
 
-  SC_PROPERTY(Keynode("action_find_identifiers"), ForceCreate)
+  SC_PROPERTY(Keynode("languages"), ForceCreate)
   static ScAddr languages;
 
-  SC_PROPERTY(Keynode("action_find_identifiers"), ForceCreate)
+  SC_PROPERTY(Keynode("concept_text_file"), ForceCreate)
   static ScAddr concept_text_file;
 
-  SC_PROPERTY(Keynode("action_find_identifiers"), ForceCreate)
+  SC_PROPERTY(Keynode("concept_message"), ForceCreate)
   static ScAddr concept_message;
 
   SC_PROPERTY(Keynode("action_interpret_non_atomic_action"), ForceCreate)
