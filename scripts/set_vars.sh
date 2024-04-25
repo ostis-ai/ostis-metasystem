a8b26873a65dbe99d3bf2510a90708f3d3459ba5
MksmOrlov <orlovmaksimkonstit@gmail.com>
2024-04-25 18:50:34 +0300

    fix: demo knowledge base and installation

--- scripts/set_vars.sh
+++ scripts/set_vars.sh
@@ -39,12 +39,12 @@ export IMS_KB_BRANCH="${IMS_KB_BRANCH:-main}"
 export IMS_KB_COMMIT="${IMS_KB_COMMIT:-""}"
 
 export STANDARD_REPO="${STANDARD_REPO:-https://github.com/ostis-ai/ostis-standard.git}"
-export STANDARD_BRANCH="${STANDARD_BRANCH:-main}"
+export STANDARD_BRANCH="${STANDARD_BRANCH:-demo}"
 export STANDARD_COMMIT="${STANDARD_COMMIT:-""}"
 export STANDARD_PATH="${STANDARD_PATH:-${METASYSTEM_PATH}/sc-models/knowledge-base/ostis-standard}"
 
 export REACT_SC_WEB_REPO="${REACT_SC_WEB_REPO:-https://github.com/ostis-ai/react-sc-web}"
-export REACT_SC_WEB_BRANCH="${REACT_SC_WEB_BRANCH:-main}"
+export REACT_SC_WEB_BRANCH="${REACT_SC_WEB_BRANCH:-demo}"
 export REACT_SC_WEB_COMMIT="${REACT_SC_WEB_COMMIT:-""}"
 export REACT_SC_WEB_PATH="${REACT_SC_WEB_PATH:-${METASYSTEM_PATH}/platform-dependent-components/interface/react-sc-web}"
 
