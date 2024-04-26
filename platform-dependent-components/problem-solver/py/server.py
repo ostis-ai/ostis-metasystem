b12e3da3680322eb1cd205337a0ce531e5d57049
Alexandr Rutkowski <kitanoyoru@protonmail.com>
2024-04-26 09:25:27 +0300

    feat: authorise agent module

--- platform-dependent-components/problem-solver/py/server.py
+++ platform-dependent-components/problem-solver/py/server.py
@@ -5,11 +5,11 @@ Distributed under the MIT License
 """
 
 import argparse
-
-from modules.clean_text_generation_module.clean_text_generation_module import \
-    CleanTextGenerationModule
-from modules.example_module.example_module import ExampleModule
 from sc_kpm import ScServer
+
+from modules.exampleModule.ExampleModule import ExampleModule
+from modules.authoriseModule.AuthoriseModule import AuthoriseModule
+
 
 SC_SERVER_PROTOCOL = "protocol"
 SC_SERVER_HOST = "host"
@@ -22,26 +22,42 @@ SC_SERVER_PORT_DEFAULT = "8090"
 
 def main(args: dict):
     server = ScServer(
-        f"{args[SC_SERVER_PROTOCOL]}://{args[SC_SERVER_HOST]}:{args[SC_SERVER_PORT]}")
+        f"{args[SC_SERVER_PROTOCOL]}://{args[SC_SERVER_HOST]}:{args[SC_SERVER_PORT]}"
+    )
 
     with server.connect():
         modules = [
             ExampleModule(),
-            CleanTextGenerationModule(),
+            AuthoriseModule(),
         ]
         server.add_modules(*modules)
         with server.register_modules():
             server.serve()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--protocol', type=str, dest=SC_SERVER_PROTOCOL, default=SC_SERVER_PROTOCOL_DEFAULT, help="Sc-server protocol")
+        "--protocol",
+        type=str,
+        dest=SC_SERVER_PROTOCOL,
+        default=SC_SERVER_PROTOCOL_DEFAULT,
+        help="Sc-server protocol",
+    )
     parser.add_argument(
-        '--host', type=str, dest=SC_SERVER_HOST, default=SC_SERVER_HOST_DEFAULT, help="Sc-server host")
+        "--host",
+        type=str,
+        dest=SC_SERVER_HOST,
+        default=SC_SERVER_HOST_DEFAULT,
+        help="Sc-server host",
+    )
     parser.add_argument(
-        '--port', type=int, dest=SC_SERVER_PORT, default=SC_SERVER_PORT_DEFAULT, help="Sc-server port")
+        "--port",
+        type=int,
+        dest=SC_SERVER_PORT,
+        default=SC_SERVER_PORT_DEFAULT,
+        help="Sc-server port",
+    )
     args = parser.parse_args()
 
     main(vars(args))
