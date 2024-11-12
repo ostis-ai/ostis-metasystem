00ace2f655e04255240a1498dcb3369f6a9b8b38
Anna Makarenko <manahafor4@gmail.com>
2024-11-12 12:37:11 +0300

    fix(config): activate env for sc-web and py-server

--- scripts/install_py_ps_deps.sh
+++ scripts/install_py_ps_deps.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/set_vars.sh"
 
-pip3 install -r "$METASYSTEM_PATH/platform-dependent-components/problem-solver/py/requirements.txt"
+python3 -m venv "${METASYSTEM_PY_SOURCES_PATH}/.venv"
+source "${METASYSTEM_PY_SOURCES_PATH}/.venv/bin/activate"
+pip3 install -r "${METASYSTEM_PY_SOURCES_PATH}/requirements.txt"
