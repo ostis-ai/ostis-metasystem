b9bd9160e0798cf726c26014baefcc3eb9c474b9
Anna Makarenko <manahafor4@gmail.com>
2024-11-12 12:37:11 +0300

    fix(config): activate env for sc-web and py-server

--- scripts/run_py_ps.sh
+++ scripts/run_py_ps.sh
@@ -3,4 +3,5 @@ set -eo pipefail
 
 source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/set_vars.sh"
 
-python3 "${METASYSTEM_PATH}/platform-dependent-components/problem-solver/py/server.py" "$@"
+# source "${METASYSTEM_PY_SOURCES_PATH}/.venv/bin/activate"
+python3 "${METASYSTEM_PY_SOURCES_PATH}/server.py" "$@"
