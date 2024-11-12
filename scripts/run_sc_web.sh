89582dc406adcdcd2aa32ee0af1c76228951c912
Anna Makarenko <manahafor4@gmail.com>
2024-11-12 12:37:11 +0300

    fix(config): activate env for sc-web and py-server

--- scripts/run_sc_web.sh
+++ scripts/run_sc_web.sh
@@ -6,4 +6,5 @@ then
   source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"/set_vars.sh
 fi
 
+source "${SC_WEB_PATH}/.venv/bin/activate"
 "${SC_WEB_PATH}/scripts/run_sc_web.sh" --allowed_origins=http://localhost:3000
