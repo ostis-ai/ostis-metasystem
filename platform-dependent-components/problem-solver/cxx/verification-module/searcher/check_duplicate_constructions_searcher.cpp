c84dd1a9adcc8497b7e8adb50e084901686530c8
Malina09 <natasha.malinovskaya.9843@gmail.com>
2024-11-25 11:37:37 +0300

    [solver] add verification module, check duplicate constr agent

---
+++ platform-dependent-components/problem-solver/cxx/verification-module/searcher/check_duplicate_constructions_searcher.cpp
@@ -0,0 +1,55 @@
+/*
+ * This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+ * Distributed under the MIT License
+ * (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include "filesystem"
+#include "optional"
+
+#include "sc-agents-common/utils/CommonUtils.hpp"
+#include "sc-memory/sc_keynodes.hpp"
+
+#include "constants/verification_constants.hpp"
+
+#include "keynodes/verification_keynodes.hpp"
+
+#include "generator/verification_generator.hpp"
+
+#include "check_duplicate_constructions_searcher.hpp"
+
+namespace verificationModule
+{
+checkDuplicateConstructionSearcher::checkDuplicateConstructionSearcher(ScMemoryContext * context)
+  : context(context)
+{
+}
+
+std::ofstream checkDuplicateConstructionSearcher::openOrCreateFile(
+    ScMemoryContext * context,
+    ScAddr & classAddr,
+    std::string const & filename)
+{
+  ScAddr fileAddr;
+  ScIterator5Ptr const & fileIterator = context->CreateIterator5(
+      classAddr,
+      ScType::ConstPermPosArc,
+      ScType::Node,
+      ScType::ConstCommonArc,
+      VerificationKeynodes::nrel_duplicate_construction_file);
+  if (fileIterator->Next())
+  {
+    fileAddr = fileIterator->Get(2);
+    std::string fileIdtf =
+        utils::CommonUtils::getIdtf(context, fileAddr, ScKeynodes::nrel_main_idtf, {VerificationConstants::LANG});
+    std::string outputFile = filename + fileIdtf;
+    return std::ofstream(outputFile, std::ios::app);
+  }
+  else
+  {
+    std::ofstream outputFile = verificationGenerator::generateDuplicationFile(context, classAddr, filename);
+    return outputFile;
+  }
+}
+
+}  // namespace verificationModule
