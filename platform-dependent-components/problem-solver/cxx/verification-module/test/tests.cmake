5d4d267ed05fae0c2f9129d1228b46531749151c
Anna Makarenko <manahafor4@gmail.com>
2024-11-25 14:36:35 +0300

    feat(problem-solver): add SearchNotEnoughSpecifiedObjectsAgent

--- platform-dependent-components/problem-solver/cxx/verification-module/test/tests.cmake
+++ platform-dependent-components/problem-solver/cxx/verification-module/test/tests.cmake
@@ -1,7 +1,7 @@
 make_tests_from_folder(${CMAKE_CURRENT_LIST_DIR}/units
     NAME verification-module-test-starter
     DEPENDS sc-memory sc-core sc-builder-lib verification-module
-    INCLUDES ${SC_MEMORY_SRC} ${SC_TOOLS_SRC} ${SC_BUILDER_PATH}
+    INCLUDES ${SC_MEMORY_SRC} ${SC_BUILDER_PATH}
 )
 
 add_definitions(-DVERIFICATION_MODULE_TEST_SRC_PATH="${CMAKE_CURRENT_LIST_DIR}")
