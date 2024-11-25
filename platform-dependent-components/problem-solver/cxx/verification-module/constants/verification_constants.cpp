5b00a851cb6ff2c52a243a161a43ab41951dc771
Malina09 <natasha.malinovskaya.9843@gmail.com>
2024-11-25 11:37:37 +0300

    feat(solver): add verification module, check duplicate constr agent

---
+++ platform-dependent-components/problem-solver/cxx/verification-module/constants/verification_constants.cpp
@@ -0,0 +1,16 @@
+/*
+ * This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+ * Distributed under the MIT License
+ * (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include "verification_constants.hpp"
+#include "keynodes/verification_keynodes.hpp"
+
+namespace verificationModule
+{
+ScAddr const VerificationConstants::LANG = VerificationKeynodes::lang_en;
+
+std::string const FileConfigs::FILE_PATH = "file_path";
+
+}  // namespace verificationModule
