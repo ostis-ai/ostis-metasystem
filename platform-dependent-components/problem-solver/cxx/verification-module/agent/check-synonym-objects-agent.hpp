20ca38a19d8befc9c9086d07d77ab564971fbe18
Andy <irarybakova90@gmial.com>
2024-12-08 19:27:58 +0300

    add synonym objects agent

---
+++ platform-dependent-components/problem-solver/cxx/verification-module/agent/check-synonym-objects-agent.hpp
@@ -0,0 +1,21 @@
+/*
+ * This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+ * Distributed under the MIT License
+ * (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+ */
+
+#pragma once
+
+#include <sc-memory/sc_agent.hpp>
+
+namespace verificationModule
+{
+class CheckSynonymObjectsAgent : public ScActionInitiatedAgent
+{
+public:
+  ScAddr GetActionClass() const override;
+
+  ScResult DoProgram(ScActionInitiatedEvent const & event, ScAction & action) override;
+};
+
+}  // namespace verificationModule
