7bd01a99b365ba474522ff70d9a0257c4c76fd2b
Andy <irarybakova90@gmial.com>
2024-12-17 14:57:01 +0300

    add synonym objects agent

--- platform-dependent-components/problem-solver/cxx/verification-module/constants/verification_constants.cpp
+++ platform-dependent-components/problem-solver/cxx/verification-module/constants/verification_constants.cpp
@@ -13,5 +13,7 @@ ScAddr const VerificationConstants::LANG = VerificationKeynodes::lang_en;
 
 std::string const FileConfigs::FILE_PATH = "file_path";
 std::string const FileConfigs::VERIFICATION_ENDPOINT = "verification";
+std::string const FileConfigs::KEY_NODE = "key_node";
+std::string const FileConfigs::TRANSLATION_NODE = "translation_node";
 
 }  // namespace verificationModule
